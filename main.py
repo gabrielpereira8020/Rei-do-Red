1ad92dc63f5519e25b9eb6f290fa9be1b1e2e007
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-05-14 13:58:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def init_services():
         )
 
             # O PULO DO GATO
-        for m in genai.list_models():
+            for m in genai.list_models():
         print(f"Modelo disponível: {m.name}")
 
         model = genai.GenerativeModel("gemini-1.5-flash")
