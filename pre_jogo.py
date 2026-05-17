b8f2d879b7277d48195887ab7fa7796d8926432a
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-05-17 00:00:31 -0300

    Update pre_jogo.py

--- pre_jogo.py
+++ pre_jogo.py
@@ -1,41 +1,53 @@
 import streamlit as st
 
+from ligas import LIGAS
+
 from api_football import (
-    buscar_estatisticas_jogo
+    buscar_jogos_da_liga
 )
 
-from ia_engine import (
-    gerar_analise_ia
-)
+from ia_engine import gerar_analise_ia
 
-from formatacao import (
-    exibir_analise
-)
+from formatacao import exibir_analise
+
+st.set_page_config(layout="wide")
 
 def tela_pre_jogo():
 
     st.header("⚽ MODO PRÉ JOGO")
 
-    jogo = st.text_input(
-        "Digite o jogo",
-        placeholder="Ex: Real Madrid x Barcelona"
+    pais = st.selectbox(
+        "🌍 Escolha o país",
+        list(LIGAS.keys())
+    )
+
+    competicoes = LIGAS[pais]
+
+    campeonato = st.selectbox(
+        "🏆 Escolha a competição",
+        list(competicoes.keys())
+    )
+
+    league_id = competicoes[campeonato]
+
+    jogos = buscar_jogos_da_liga(league_id)
+
+    lista_jogos = [
+        jogo["nome"] for jogo in jogos
+    ]
+
+    jogo_escolhido = st.selectbox(
+        "⚽ Escolha o jogo",
+        lista_jogos
     )
 
     if st.button("🔥 GERAR ANÁLISE"):
 
-        if jogo.strip() == "":
-            st.warning("Digite um jogo.")
-            return
-
-        with st.spinner("Buscando dados da API Football..."):
-
-            dados = buscar_estatisticas_jogo(jogo)
-
         with st.spinner("IA analisando partida..."):
 
             resposta = gerar_analise_ia(
-                jogo,
-                dados
+                jogo_escolhido,
+                jogos
             )
 
         exibir_analise(resposta)
