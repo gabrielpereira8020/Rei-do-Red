cafb67e91d723b4dc65896d3a33a82a945793b3f
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-05-16 23:59:02 -0300

    Create ligas.py

---
+++ ligas.py
@@ -0,0 +1,75 @@
+LIGAS = {
+
+    "Brasil": {
+        "Campeonato Brasileiro Série A": 71,
+        "Campeonato Brasileiro Série B": 72
+    },
+
+    "Inglaterra": {
+        "Premier League": 39,
+        "EFL Championship": 40
+    },
+
+    "Espanha": {
+        "LaLiga": 140,
+        "LaLiga 2": 141
+    },
+
+    "Itália": {
+        "Serie A": 135,
+        "Serie B": 136
+    },
+
+    "Alemanha": {
+        "Bundesliga": 78,
+        "2. Bundesliga": 79
+    },
+
+    "França": {
+        "Ligue 1": 61,
+        "Ligue 2": 62
+    },
+
+    "Portugal": {
+        "Primeira Liga": 94,
+        "Segunda Liga": 95
+    },
+
+    "Argentina": {
+        "Liga Profesional": 128,
+        "Primera Nacional": 129
+    },
+
+    "Estados Unidos": {
+        "MLS": 253,
+        "USL Championship": 254
+    },
+
+    "Arábia Saudita": {
+        "Saudi Pro League": 307,
+        "Saudi First Division": 308
+    },
+
+    "Japão": {
+        "J1 League": 98,
+        "J2 League": 99
+    },
+
+    "Competições Internacionais": {
+
+        "UEFA Champions League": 2,
+        "UEFA Europa League": 3,
+        "UEFA Conference League": 848,
+
+        "Libertadores": 13,
+        "Sudamericana": 11,
+
+        "CONCACAF Champions Cup": 16,
+
+        "AFC Champions League Elite": 17,
+
+        "CAF Champions League": 12,
+
+        "Mundial de Clubes FIFA": 15
+    }
+}
