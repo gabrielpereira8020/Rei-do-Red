9a2864f24ede4a009831a608a424bb12ed0a2e24
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-05-19 15:09:55 -0300

    Update ia_engine.py

--- ia_engine.py
+++ ia_engine.py
@@ -55,7 +55,7 @@ FIM
 """
     try:
         response = client.models.generate_content(
-            model="models/gemini-2.5-flash-lite",
+            model="models/gemini-3.1-flash-lite",
             contents=prompt
         )
         return response.text
