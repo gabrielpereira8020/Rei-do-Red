8e7c763d4e323ef88ec2a1afd778907dac6055b3
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-05-30 12:15:09 -0300

    Update ligas.py

--- ligas.py
+++ ligas.py
@@ -1,75 +1,60 @@
 LIGAS = {
-
     "Brasil": {
         "Serie A": 71,
-        "Serie B": 72
+        "Serie B": 72,
+        "Serie C": 73
     },
-
     "Inglaterra": {
         "Premier League": 39,
         "EFL Championship": 40
     },
-
     "Espanha": {
         "LaLiga": 140,
         "LaLiga 2": 141
     },
-
-    "Itália": {
+    "Italia": {
         "Serie A": 135,
         "Serie B": 136
     },
-
     "Alemanha": {
         "Bundesliga": 78,
         "2. Bundesliga": 79
     },
-
-    "França": {
+    "Franca": {
         "Ligue 1": 61,
         "Ligue 2": 62
     },
-
     "Portugal": {
         "Primeira Liga": 94,
         "Segunda Liga": 95
     },
-
     "Argentina": {
         "Liga Profesional": 128,
         "Primera Nacional": 129
     },
-
     "Estados Unidos": {
         "MLS": 253,
         "USL Championship": 254
     },
-
-    "Arábia Saudita": {
-        "Saudi Pro League": 307,
-        "Saudi First Division": 308
+    "Arabia Saudita": {
+        "Saudi Pro League": 307
     },
-
-    "Japão": {
+    "Japao": {
         "J1 League": 98,
         "J2 League": 99
     },
-
-    "Competições Internacionais": {
-
+    "Competicoes Internacionais": {
+        "Copa do Mundo": 1,
         "UEFA Champions League": 2,
         "UEFA Europa League": 3,
         "UEFA Conference League": 848,
-
         "Libertadores": 13,
         "Sudamericana": 11,
-
+        "Mundial de Clubes FIFA": 15,
         "CONCACAF Champions Cup": 16,
-
         "AFC Champions League Elite": 17,
-
         "CAF Champions League": 12,
-
-        "Mundial de Clubes FIFA": 15
+        "Amistosos Selecoes": 9,
+        "Amistosos Clubes": 667
     }
 }
