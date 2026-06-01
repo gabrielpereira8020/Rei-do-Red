100844fddf1c9b21aa2c66281fd484d4fe110d69
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-06-01 00:45:56 -0300

    Update alavancagem.py

--- alavancagem.py
+++ alavancagem.py
@@ -110,6 +110,7 @@ def buscar_odds_evento(event_id, odds_api_key):
             return ""
 
         data = r.json()
+        st.write(data)
         bookmakers = data.get("bookmakers", {})
         if not bookmakers:
             return ""
