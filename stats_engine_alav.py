1a17d3d4e43a75934523470022ad6c759d255e4f
gabrielpereira8020 <gabriel.pereira8020@gmail.com>
2026-06-06 11:48:10 -0300

    Update stats_engine_alav.py

--- stats_engine_alav.py
+++ stats_engine_alav.py
@@ -30,11 +30,12 @@ def _todos_ids():
 
 # Ligas de alto valor para prioridade 1
 LIGAS_ALTA_IDS = {
-    39, 61, 71, 78, 94, 135, 140, 848,  # ligas nacionais top
-    2, 3, 13, 11, 15, 1, 16, 17, 12, 9,     # competicoes internacionais
-    88, 144, 203, 179, 262, 103, 113, 119 
-    207
+    39, 61, 71, 78, 94, 135, 140, 848,
+    2, 3, 13, 11, 15, 1, 16, 17, 12,
+    9,
+    88, 144, 203, 179, 262, 103, 113, 119
 }
+
 # Tudo mais do ligas.py recebe prioridade 2
 LIGAS_MEDIA_IDS = _todos_ids() - LIGAS_ALTA_IDS
 
